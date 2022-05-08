4f8a15370981c7f6e503ea0c3c3edcfd66a71c14
Neeraj Maurya <mauryaneeraj11@gmail.com>
2022-05-09 02:09:27 +0530

    add hero and nav

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,21 +1,47 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { add } from "../features/sampleSlice";
+import { Blob1, Blob2, Blob3, LogoMain, ScrollIcon } from "./AssestsExport";
+import Navbar from "./Navbar";
 
 const Hero = () => {
-  const { count } = useSelector((state) => state.sample);
-  const dispatch = useDispatch();
-
   return (
-    <div className="text-white bg-red-400 text-center py-3 font-medium">
-      <p>Hello Treasuro</p>
-      <span>{count ?? 0}</span>
-      <button
-        onClick={() => dispatch(add())}
-        className="border-2 border-solid mx-4 text-blue-500"
-      >
-        Add Num
-      </button>
+    <div className="relative h-[95vh] md:h-screen overflow-hidden bg-purple-primary">
+      <Navbar />
+      {/* center part */}
+      <div className="relative z-10 grid place-items-center mt-[10vh] md:mt-[16vh]">
+        <img
+          src={LogoMain}
+          className=" w-[200px] md:w-[360px] h-auto"
+          alt="Treasuro 2022"
+        />
+        <p className="max-w-[160px] text-[20px] mt-[30px] tracking-wider text-center text-white font-bold">
+          Are you ready for a Hunt?
+        </p>
+        <p className="text-xl md:text-2xl md:text-[35px] mt-[15px] font-black tracking-wider text-yellow ">
+          And win prizes
+        </p>
+      </div>
+      <div className="text-center">
+        <img
+          src={ScrollIcon}
+          className="mx-auto mt-12 w-6  relative z-10"
+          alt="scroll icon"
+        />
+      </div>
+      {/* blobs */}
+      <img
+        className="absolute top-0 left-0 w-80 h-auto"
+        src={Blob3}
+        alt="top left blob"
+      />
+      <img
+        className="absolute -right-20 top-32 md:top-16 h-[280px]"
+        src={Blob2}
+        alt="top right blob"
+      />
+      <img
+        className="absolute -bottom-0 md:-bottom-[100px] w-full"
+        src={Blob1}
+        alt="bottom blob"
+      />
     </div>
   );
 };
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Cross, Hamburger, LogoWhite } from "./AssestsExport";
+import NavLinks from "./NavLinks";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(true);
+  return (
+    <nav className="z-10 flex justify-between items-center mx-auto my-6 w-[90vw] text-white font-semibold">
+      <Link to="/" className="z-10">
+        <img className="cursor-pointer w-24 z-10" src={LogoWhite} alt="" />
+      </Link>
+      <div className="hidden z-10 md:flex justify-between items-center w-full ml-[100px]">
+        <NavLinks />
+      </div>
+      <div className="md:hidden z-10">
+        <img
+          src={Hamburger}
+          onClick={() => setShowMenu(true)}
+          className="w-9 cursor-pointer"
+          alt=""
+        />
+      </div>
+      {/* SideMenu */}
+      {showMenu && (
+        <div
+          className="absolute z-20 bg-black opacity-40 top-0 left-0 right-0 bottom-0 w-screen h-screen"
+          onClick={() => setShowMenu(false)}
+        ></div>
+      )}
+      <div
+        className={`absolute z-30 bg-purple-tertiary h-screen w-80 top-0 left-0 ease-linear duration-200 
+        ${!showMenu && "-translate-x-full"}`}
+      >
+        <div className="flex justify-end mx-5 my-6">
+          <img
+            src={Cross}
+            onClick={() => setShowMenu(false)}
+            className="w-10 cursor-pointer"
+            alt="cross"
+          />
+        </div>
+        <div className="flex flex-col m-7 space-y-10">
+          <NavLinks />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
