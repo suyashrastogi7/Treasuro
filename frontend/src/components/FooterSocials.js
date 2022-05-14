14ee8435b8a814b672d0b0b4ba6abc9433ebb79e
Neeraj Maurya <mauryaneeraj11@gmail.com>
2022-05-14 23:00:40 +0530

    add target att

--- frontend/src/components/FooterSocials.js
+++ frontend/src/components/FooterSocials.js
@@ -2,28 +2,32 @@ import { Discord, Github, Linkedin, Mail } from "./AssetsExport";
 const FooterSocials = () => {
   return (
     <div className="flex justify-between mt-16 md:mt-24 w-full md:w-56 ">
-      <a href="https://github.com/MMIL">
+      <a href="https://github.com/MMIL" target="_blank" rel="noreferrer">
         <img
           className="w-8 ease-linear duration-200 hover:scale-110"
           src={Github}
           alt=""
         />
       </a>
-      <a href="https://www.linkedin.com/company/mmil">
+      <a
+        href="https://www.linkedin.com/company/mmil"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
           className="w-8 ease-linear duration-200 hover:scale-110"
           src={Linkedin}
           alt=""
         />
       </a>
-      <a href="mailto:mmiljss@gmail.com">
+      <a href="mailto:mmiljss@gmail.com" target="_blank" rel="noreferrer">
         <img
           className="w-8 ease-linear duration-200 hover:scale-110"
           src={Mail}
           alt=""
         />
       </a>
-      <a href="https://discord.gg/6xDkYAEDx3">
+      <a href="https://discord.gg/6xDkYAEDx3" target="_blank" rel="noreferrer">
         <img
           className="w-8 ease-linear duration-200 hover:scale-110"
           src={Discord}
