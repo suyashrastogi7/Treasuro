55121b0cb0e99c88024fc9334e575c2e60353f80
Neeraj Maurya <mauryaneeraj11@gmail.com>
2022-05-14 21:58:53 +0530

    fix ticket align

--- frontend/src/components/Tickets.js
+++ frontend/src/components/Tickets.js
@@ -6,11 +6,11 @@ const Tickets = () => {
   return (
     <div className="bg-purple-primary px-4 md:px-16 py-32">
       <TitleDash title="why to buy tickets?" />
-      <p className="text-3xl md:text-[64px] font text-lime font-bold mt-8 mb-12">
+      <p className="text-3xl md:text-[64px] font text-lime font-bold mt-12 mb-12">
         Tickets?
       </p>
       {/* ticket */}
-      <div className="flex flex-col lg:flex-row items-center lg:items-stretch lg:justify-start mt-12 md:mt-24 w-full mx-auto">
+      <div className="flex flex-col lg:flex-row items-center lg:items-stretch lg:justify-center mt-12 md:mt-24 w-full mx-auto">
         <div className="bg-base-primary rounded px-8 py-12 w-full sm:w-10/12 md:w-[500px] ">
           <div className="text-md  font-semibold text-white uppercase tracking-wide">
             <p>three-uses-only</p>
