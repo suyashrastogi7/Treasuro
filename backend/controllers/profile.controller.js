1ffe62fd8fbfd687b5d911d15fd82d558f04f30b
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-06-21 02:28:51 +0530

    Added Get Profile

--- backend/controllers/profile.controller.js
+++ backend/controllers/profile.controller.js
@@ -1,7 +1,33 @@
 // Routes for logged in user's profile
-const getProfile = (req, res) => {
-  return res.status(200).json({ user: req.user });
+const User = require("../models/user_schema");
+const jwt = require("jsonwebtoken");
+const showError = require("../utils/showError.js");
+
+const getProfile = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const id = jwt.decode(token)?.user.id;
+        const user = await User.findOne({
+            _id: id,
+        });
+        res.status(200).json({
+            user: {
+                name: user.name,
+                username: user.username,
+                email: user.email,
+                rollno: user.rollno,
+                phoneno: user.phoneno,
+                attempts: user.attempts,
+                active: user.active,
+                score: user.score,
+                level: user.level,
+                image: user.image,
+            },
+        });
+    } catch (error) {
+        showError(error, res);
+    }
 };
 module.exports = {
-  getProfile,
+    getProfile,
 };
