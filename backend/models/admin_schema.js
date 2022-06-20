e74a795ce2abd068cc9b10d8f0e1874f39846b95
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-06-21 02:25:41 +0530

    Authentication Tested

--- backend/models/admin_schema.js
+++ backend/models/admin_schema.js
@@ -1,42 +1,39 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 adminSchema.pre("save", async function (next) {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    this.password = hashedPassword;
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
 
-    next();
-  } catch (err) {
-    next(err);
-  }
+        next();
+    } catch (err) {
+        next(err);
+    }
 });
 
-adminSchema.methods.isValidPassword = function(password){
-  try{
-
-    return await bcrypt.compare(password, this.password);
-
-  }catch(error){
-    console.log(error);
-    next(error)
-  }
-
-}
+adminSchema.methods.isValidPassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
 
 module.exports = mongoose.model("Admin", adminSchema);
