eacb44cc70aec7bd11302f6b236c686aced19fe0
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-06-24 07:40:00 +0530

    Finalized Leaderboard

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -1,47 +1,70 @@
+import React, { useState, useEffect } from "react";
 import { v4 } from "uuid";
+import axios from "axios";
 import RankRow from "../components/RankRow";
 import Template from "../components/Template";
 import TitleDash from "../components/TitleDash";
 
-const data = {
-  top50: [
-    { pos: 1, name: "MMIL", pts: 154 },
-    { pos: 2, name: "Hello2", pts: 123 },
-    { pos: 3, name: "Hello3", pts: 102 },
-    { pos: 4, name: "Hello4", pts: 98 },
-    { pos: 5, name: "Hello5", pts: 97 },
-  ],
-  mine: { pos: 1, name: "MMIL", pts: 154 },
-};
-
 const Leaderboard = () => {
-  const List = data.top50?.map((el, idx) => (
-    <RankRow {...el} idx={idx} key={v4()} />
-  ));
-  const You = data?.mine && (
-    <RankRow
-      pos={data.mine.pos}
-      name={data.mine.name}
-      pts={data.mine.pts}
-      idx={-1}
-      spec="border-2 border-white mb-6 mt-8"
-    />
-  );
+    const [data, setData] = useState();
 
-  return (
-    <Template>
-      <div>
-        <TitleDash title="leaderboard" />
-      </div>
-      <div className="flex mt-12 mb-6 text-lg font-black text-white md:text-xl">
-        <p className="w-3/12 text-center md:w-1/12">Pos</p>
-        <p className="w-6/12 pl-4 md:w-10/12">Name</p>
-        <p className="w-3/12 text-center md:w-1/12">Points</p>
-      </div>
-      <div>{You}</div>
-      <div>{List}</div>
-    </Template>
-  );
+    async function getLeaderboard() {
+        try {
+            let config = {
+                headers: {
+                    Authorization:
+                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJiMDg3NDU5MjUxZjkyNDRjN2U5ZmY5In0sImlhdCI6MTY1NjAxNTA3NiwiZXhwIjoxNjU2MTAxNDc2fQ.NxKb5Zqz9HLiwEkzRchVVB7tPz6wXm4MwbitFx_3iqw",
+                },
+            };
+            const { data } = await axios.get(
+                "http://localhost:5000/api/leaderboard/",
+                config
+            );
+            console.log(data);
+            setData(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getLeaderboard();
+    }, []);
+
+    const List = data?.leaderboard?.map((el, idx) => (
+        <RankRow
+            pos={idx + 1}
+            idx={idx}
+            pts={el.pts}
+            name={el.name}
+            key={v4()}
+        />
+    ));
+
+    const You = data?.mine && (
+        <RankRow
+            pos={data.mine.pos}
+            name={data.mine.name}
+            pts={data.mine.pts}
+            idx={-1}
+            spec="border-2 border-white mb-6 mt-8"
+        />
+    );
+
+    return (
+        <Template>
+            <div>
+                <TitleDash title="leaderboard" />
+            </div>
+            <div className="flex mt-12 mb-6 text-lg font-black text-white md:text-xl">
+                <p className="w-3/12 text-center md:w-1/12">Pos</p>
+                <p className="w-6/12 pl-4 md:w-10/12">Name</p>
+                <p className="w-3/12 text-center md:w-1/12">Points</p>
+            </div>
+            <div>{You}</div>
+            <div>{List}</div>
+        </Template>
+    );
 };
 
 export default Leaderboard;
