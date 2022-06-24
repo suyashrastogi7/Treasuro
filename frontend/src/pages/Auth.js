391728968572b2493e1220f284318e9215a8faf9
krapirastogi <krapirastogi@gmail.com>
2022-06-25 00:30:51 +0530

    signup and login button

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -8,15 +8,34 @@ import TitleDash from "../components/TitleDash";
 import Template from "../components/Template";
 import Input from "../components/Input";
 
+
 const Auth = () => {
+    const [active,setActive]=useState("Login");
+
     const [formState, setFormState] = useState({
         username: "",
         password: "",
         rememberMe: false,
     });
+    const [signState, setSignState] = useState({
+        
+        name:"",
+        username: "",
+        password: "",
+        admissionno:"",
+        email:"",
+        phoneno:"",
+        rememberMe: false,
+    });
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
-
+    const switchtoLogin=()=>{
+        setActive("Login");
+    }
+    const switchtoSignUp=()=>{
+        setActive("SignUp");
+    }
     return (
         <Template>
             <div>
@@ -28,9 +47,28 @@ const Auth = () => {
                     You can win a ton of a prizes and get a chance to win
                     exclusive swags
                 </p>
+            
             </div>
+            
             <div className="flex flex-col justify-center items-center my-9">
-                <div className="w-1/4">
+                <div title="buttons" className="">
+                <button className="bg-[#98FF9D] flex-row cursor-pointer  justify-center items-center px-3 py-2 md:px-6 md:py-2 my-4" onClick={switchtoLogin}>
+                    
+                 <div className="text-md md:text-lg lg:text-xl text-black md:font-black">
+                    
+                 Login
+                 </div>
+                </button>
+                
+                <button className="bg-[#FFA800] flex-row cursor-pointer  justify-center items-center px-3 py-2 md:px-6 md:py-2 my-4" onClick={switchtoSignUp}>
+                
+                <div className="text-md md:text-lg lg:text-xl text-black md:font-black">
+                SignUp
+                </div>
+                </button>
+                </div>
+               {active === "Login" && 
+                (<div className="w-1/4">
                     <TitleDash title="LOGIN" className="my-9" />
                     <Input
                         type="text"
@@ -60,7 +98,7 @@ const Auth = () => {
                         label="Remember Me"
                         name="remember me"
                     />
-                    <Link to="/signin" className="mx-4 mt-3">
+                     <Link to="/signin" className="mx-4 mt-3">
                         <button
                             onClick={handleOnClick}
                             className="flex justify-between items-center cursor-pointer rounded-2xl px-3 py-2 md:px-6 md:py-2 my-4 bg-hot-pink font-semibold"
@@ -70,6 +108,89 @@ const Auth = () => {
                         </button>
                     </Link>
                 </div>
+                    )
+               }
+                    {active === "SignUp" && (
+                      <div className="w-1/4">
+                      <TitleDash title="SignUp" className="my-9" />
+                      <Input
+                          type="text"
+                          signValue={signState.name}
+                          signState={signState}
+                          setValue={setSignState}
+                          label="Name"
+                          Placeholder="Neeraj Maurya"
+                          name="name"
+                      />
+                      
+                      <Input
+                          type="text"
+                          signValue={signState.username}
+                          signState={signState}
+                          setValue={setSignState}
+                          label="Username"
+                          Placeholder="seiken420"
+                          name="username"
+                      />
+                     
+                      <Input
+                          type="password"
+                          signValue={signState.password}
+                          signState={signState}
+                          label="Password"
+                          setValue={setSignState}
+                          Placeholder="****"
+                          name="password"
+                      />
+                      <Input
+                          type="text"
+                          signValue={signState.admissionno}
+                          signState={signState}
+                          setValue={setSignState}
+                          label="Admission Number"
+                          Placeholder="20CS110"
+                          name="admissionno"
+                      />
+                      <Input
+                          type="email"
+                          signValue={signState.name}
+                          signState={signState}
+                          setValue={setSignState}
+                          label="Email"
+                          Placeholder="pamaria@gmail.com"
+                          name="email"
+                      />
+                      <Input
+                      type="number"
+                      signValue={signState.phoneno}
+                      signState={signState}
+                      setValue={setSignState}
+                      label="Phone Number"
+                      Placeholder="9876543210"
+                      name="phoneno"
+                  />
+                      <Input
+                          type="checkbox"
+                          signValue={signState.rememberMe}
+                          signState={signState}
+                          setValue={setSignState}
+                          Placeholder="Remember Me"
+                          label="Remember Me"
+                          name="remember me"
+                      />
+                       <Link to="/signin" className="mx-4 mt-3">
+                          <button
+                              onClick={handleOnClick}
+                              className="flex justify-between items-center cursor-pointer rounded-2xl px-3 py-2 md:px-6 md:py-2 my-4 bg-hot-pink font-semibold"
+                          >
+                              <span className="mr-1 text-white">Login</span>
+                              <img src={Arrow} alt="arrow" />
+                          </button>
+                      </Link>
+                  </div>
+  
+                    )} 
+                   
             </div>
         </Template>
     );
