50aa72b82f53bea30090243d3f7aa493ba4734e5
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-07-07 02:12:07 +0530

    Implemented SCanner, Has issues

--- frontend/src/pages/Question.js
+++ frontend/src/pages/Question.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { QrReader } from "react-qr-reader";
+import QrScan from "react-qr-reader";
 
 import Template from "../components/Template";
 import TitleDash from "../components/TitleDash";
@@ -8,7 +8,7 @@ import Loader from "../components/Loader";
 import { useSelector, useDispatch } from "react-redux";
 import { answer, question } from "../features/questionSlice";
 
-import { Qr } from "../components/AssetsExport";
+// import { Qr } from "../components/AssetsExport";
 import { alertActions } from "../features/alertSlice";
 
 const Question = () => {
@@ -25,11 +25,7 @@ const Question = () => {
     const getQuestion = useSelector((state) => state.question);
 
     const [data, setData] = useState("No result");
-    const [openHandler, setOpenHandler] = useState(false);
 
-    function openQR() {
-        setOpenHandler(true);
-    }
     return (
         <Template>
             <Loader loading={loading} />
@@ -40,38 +36,36 @@ const Question = () => {
                 </h1>
                 <p className="text-white text-xl my-8">Scan QR to answer : </p>
             </div>
-            <div>
-                {openHandler && (
-                    <QrReader
-                        constraints={{
-                            facingMode: "environment",
-                        }}
-                        onResult={(result, error) => {
-                            if (!!result) {
-                                setData(result?.text);
-                                dispatch(answer(token, data));
-                                dispatch(
-                                    alertActions.createAlert({
-                                        status: success,
-                                        message: message.message,
-                                    })
-                                );
-                            }
-
-                            if (!!error) {
-                                console.info(error);
-                            }
-                        }}
-                        style={{ width: "100%" }}
-                    />
-                )}
-                <button
-                    onClick={() => openQR}
-                    className="flex justify-between items-center relative mx-auto px-9 py-3 bg-white rounded-xl hover:scale-105 hover:-translate-y-1 transition ease-in-out hover:shadow-md"
-                >
+            <div className="mx-auto">
+                <QrScan
+                    constraints={{
+                        facingMode: "environment",
+                    }}
+                    onScan={(result) => {
+                        if (!!result) {
+                            setData(result);
+                            dispatch(answer(token, data));
+                            dispatch(
+                                alertActions.createAlert({
+                                    status: success,
+                                    message: message.message,
+                                })
+                            );
+                        }
+                    }}
+                    onLoad={(result) => {
+                        console.log(result);
+                    }}
+                    onError={(error) => {
+                        console.log(error);
+                    }}
+                    style={{ height: 250, width: 250 }}
+                    className="mx-auto"
+                />
+                {/* <button className="flex mt-9 justify-between items-center relative mx-auto px-9 py-3 bg-white rounded-xl hover:scale-105 hover:-translate-y-1 transition ease-in-out hover:shadow-md">
                     <p className="text-xl align-middle">Scan and Answer</p>
                     <img src={Qr} alt="Scan QR" className="h-12 w-12 ml-4" />
-                </button>
+                </button> */}
             </div>
         </Template>
     );
